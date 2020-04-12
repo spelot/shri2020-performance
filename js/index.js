0fdddcd1aff3019894343097b3cfb9bbc9b9d95e
Andrew Vyatkin <5979694+spelot@users.noreply.github.com>
2020-04-12 18:53:07 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,1 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const multimediaScript = document.createElement('script');
-    multimediaScript.setAttribute('src','./js/multimedia.js');
-    const buttonsContainer = document.querySelector('.buttons-wrap');
-
-    buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-        '<button class="button">Нет</button>';
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
-});
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.setAttribute("src","./js/multimedia.js"),document.querySelector(".buttons-wrap").innerHTML='<button class="button button_yellow" type="button">Да</button><button class="button">Нет</button>',document.querySelector(".multimedia").appendChild(t)});
